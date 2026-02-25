adf7714a01750a87dd661a0c0517c9502f0f9cdb
Aulia Noviyansyah Sochib <64592636+viyansyah@users.noreply.github.com>
2026-02-25 12:45:33 +0700

    Refactor server to use environment variable for port

--- server/app.js
+++ server/app.js
@@ -92,6 +92,8 @@ io.on('connection', (socket) => {
 
 });
 
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
